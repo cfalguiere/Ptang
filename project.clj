f7237fdced32ee7a3a46afb2f0a39a6cdb352c5f
Claude Falguiere <cfalguiere@gmail.com>
2012-10-10 22:26:37 +0200

    configured project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
-(defproject Ptang "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+(defproject Ptang "0.1.0-SNAPSHOT"
+  :description "Analysis tool for performance test results"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [incanter "1.3.0"]]
+  :dev-dependencies [[swank-clojure "1.4.2"][lein-midje "1.0.10"][com.stuartsierra/lazytest "1.2.3"]]
+  :repositories {"stuart" "http://stuartsierra.com/maven2"}
+  )
