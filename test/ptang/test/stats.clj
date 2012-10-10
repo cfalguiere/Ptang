472aeebfc2f6e481a570a27516552faa2adbaeb8
Claude Falguiere <cfalguiere@gmail.com>
2012-10-10 23:19:32 +0200

    changed sample dataset implementation

--- test/ptang/test/stats.clj
+++ test/ptang/test/stats.clj
@@ -18,10 +18,8 @@
 ;    (is (= 5.0 (:max result)))
 ;    ))
 
-(def sample-dataset 
-  (incanter/dataset [:t] [{:t 5}{:t 5}] ))
-
 ;;; assumes that incanter computation is correct. Only test the data structure
 (fact "perf summary when 2 lines and both values are 5.0"
-      (perf-summary sample-dataset)  =>
-      {:count 2 :mean 5.0 :sd 0.0 :min 5.0 :q95 5.0 :max 5.0})
+      (let [ds (incanter/dataset [:t] [{:t 5}{:t 5}] )]
+	(perf-summary ds)  =>
+	{:count 2 :mean 5.0 :sd 0.0 :min 5.0 :q95 5.0 :max 5.0}))
