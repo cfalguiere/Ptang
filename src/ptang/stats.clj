b3ef95f1bd5df28f2d225ab3b6cee15ca3b48432
Claude Falguiere <cfalguiere@gmail.com>
2012-10-10 23:56:13 +0200

    added assert / http detail

--- src/ptang/stats.clj
+++ src/ptang/stats.clj
@@ -5,9 +5,14 @@
   (:use [incanter.charts :only [bar-chart histogram time-series-plot add-lines set-stroke-color set-stroke]]))
 
 (defn run-summary [ds]
-  (let [ success ($where {:s true :rc {:$in #{200 304}} } ds)
-	nbErrors (- (nrow ds) (nrow success)) ] 
-    { :count 2 :errorCount nbErrors}
+  (let [ assertSuccess ($where {:s true} ds)
+	httpSuccess ($where {:rc {:$in #{200 304}}} ds)
+	success ($where {:s true :rc {:$in #{200 304}}} ds)
+	allCount (nrow ds)] 
+  { :count 2
+   :errorCount (- allCount (nrow success))
+   :assertErrorCount (- allCount (nrow assertSuccess))
+   :httpErrorCount (- allCount (nrow httpSuccess)) }
   )) 
 
 (defn response-time-summary [ds]
--- test/ptang/test/stats.clj
+++ test/ptang/test/stats.clj
@@ -23,21 +23,21 @@
 				 [ {:t 5 :s true :rc 200}
 				   {:t 5 :s true :rc 200}]) ]
 	(run-summary ds)  =>
-	{:count 2 :errorCount 0}))
+	{:count 2 :errorCount 0 :assertErrorCount 0 :httpErrorCount 0}))
 
 (fact "run summary when 2 lines and 1 is an assert failure"
       (let [ds (incanter/dataset [:t :s :rc]
 				 [ {:t 5 :s true :rc 200}
 				   {:t 5 :s false :rc 200}]) ]
 	(run-summary ds)  =>
-	{:count 2 :errorCount 1}))
+	{:count 2 :errorCount 1 :assertErrorCount 1 :httpErrorCount 0}))
 
 (fact "run summary when 2 lines and 1 is an http failure"
       (let [ds (incanter/dataset [:t :s :rc]
 				 [ {:t 5 :s true :rc 200}
 				   {:t 5 :s true :rc 500}]) ]
 	(run-summary ds)  =>
-	{:count 2 :errorCount 1}))
+	{:count 2 :errorCount 1 :assertErrorCount 0 :httpErrorCount 1}))
 
 ;;; assumes that incanter functions are correct. Only test the data structure
 (fact "response time summary when 2 lines and both values are 5.0"
