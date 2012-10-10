9060533ccf24b0396841200b6f46b7432bb7371b
Claude Falguiere <cfalguiere@gmail.com>
2012-10-10 23:48:44 +0200

    added run summary (count and error count)

--- src/ptang/stats.clj
+++ src/ptang/stats.clj
@@ -1,8 +1,14 @@
 (ns ptang.stats
-  (:use [incanter.core :only [$ $data with-data sel view $rollup]])
+  (:use [incanter.core :only [$ $where $data nrow with-data sel view $rollup]])
   (:use [incanter.io :only [read-dataset]])
   (:use [incanter.stats :only [mean sd quantile]])
   (:use [incanter.charts :only [bar-chart histogram time-series-plot add-lines set-stroke-color set-stroke]]))
+
+(defn run-summary [ds]
+  (let [ success ($where {:s true :rc {:$in #{200 304}} } ds)
+	nbErrors (- (nrow ds) (nrow success)) ] 
+    { :count 2 :errorCount nbErrors}
+  )) 
 
 (defn response-time-summary [ds]
   (zipmap [ :count :mean :sd :min :q95 :max]
--- test/ptang/test/stats.clj
+++ test/ptang/test/stats.clj
@@ -18,8 +18,30 @@
 ;    (is (= 5.0 (:max result)))
 ;    ))
 
+(fact "run summary when 2 lines and both are success"
+      (let [ds (incanter/dataset [:t :s :rc]
+				 [ {:t 5 :s true :rc 200}
+				   {:t 5 :s true :rc 200}]) ]
+	(run-summary ds)  =>
+	{:count 2 :errorCount 0}))
+
+(fact "run summary when 2 lines and 1 is an assert failure"
+      (let [ds (incanter/dataset [:t :s :rc]
+				 [ {:t 5 :s true :rc 200}
+				   {:t 5 :s false :rc 200}]) ]
+	(run-summary ds)  =>
+	{:count 2 :errorCount 1}))
+
+(fact "run summary when 2 lines and 1 is an http failure"
+      (let [ds (incanter/dataset [:t :s :rc]
+				 [ {:t 5 :s true :rc 200}
+				   {:t 5 :s true :rc 500}]) ]
+	(run-summary ds)  =>
+	{:count 2 :errorCount 1}))
+
 ;;; assumes that incanter functions are correct. Only test the data structure
 (fact "response time summary when 2 lines and both values are 5.0"
       (let [ds (incanter/dataset [:t] [{:t 5}{:t 5}] )]
 	(response-time-summary ds)  =>
 	{:count 2 :mean 5.0 :sd 0.0 :min 5.0 :q95 5.0 :max 5.0}))
+
