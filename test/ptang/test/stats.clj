8ccd7ca1e46a490befb73c7f1eec6cb2f7424f1a
Claude Falguiere <cfalguiere@gmail.com>
2012-10-11 00:06:04 +0200

    refactoring of the test

--- test/ptang/test/stats.clj
+++ test/ptang/test/stats.clj
@@ -4,25 +4,32 @@
   (:require [incanter.core :as incanter]))
 
 (fact "run summary when 2 lines and both are success"
-      (let [ds (incanter/dataset [:t :s :rc]
-				 [ {:t 5 :s true :rc 200}
-				   {:t 5 :s true :rc 200}]) ]
+      (let [ds (incanter/dataset [:s :rc]
+				 [ { :s true :rc 200}
+				   {:s true :rc 200}]) ]
 	(run-summary ds)  =>
 	{:count 2 :errorCount 0 :assertErrorCount 0 :httpErrorCount 0}))
 
 (fact "run summary when 2 lines and 1 is an assert failure"
-      (let [ds (incanter/dataset [:t :s :rc]
-				 [ {:t 5 :s true :rc 200}
-				   {:t 5 :s false :rc 200}]) ]
+      (let [ds (incanter/dataset [:s :rc]
+				 [ {:s true :rc 200}
+				   {:s false :rc 200}]) ]
 	(run-summary ds)  =>
 	{:count 2 :errorCount 1 :assertErrorCount 1 :httpErrorCount 0}))
 
 (fact "run summary when 2 lines and 1 is an http failure"
-      (let [ds (incanter/dataset [:t :s :rc]
-				 [ {:t 5 :s true :rc 200}
-				   {:t 5 :s true :rc 500}]) ]
+      (let [ds (incanter/dataset [:s :rc]
+				 [ {:s true :rc 200}
+				   {:s true :rc 500}]) ]
 	(run-summary ds)  =>
 	{:count 2 :errorCount 1 :assertErrorCount 0 :httpErrorCount 1}))
+
+(fact "run summary when 2 lines and 1 is an http failure and an assert failure"
+      (let [ds (incanter/dataset [:s :rc]
+				 [ {:s true :rc 200}
+				   {:s false :rc 500}]) ]
+	(run-summary ds)  =>
+	{:count 2 :errorCount 1 :assertErrorCount 1 :httpErrorCount 1}))
 
 ;;; assumes that incanter functions are correct. Only test the data structure
 (fact "response time summary when 2 lines and both values are 5.0"
