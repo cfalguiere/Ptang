06472c8e85be511875b2c2eccf015849b84eb25f
Claude Falguiere <cfalguiere@gmail.com>
2013-06-01 16:15:37 +0200

    added test harness - https://github.com/cfalguiere/Ptang/issues/8

---
+++ PtangDSL/Ptang/test/Ptang/test/charts.clj
@@ -0,0 +1,21 @@
+(ns Ptang.test.charts
+  (:use [ptang.charts])
+  (:use [midje.sweet])
+  (:use [incanter.io :only [read-dataset]])
+  (:require [incanter.core :as incanter]))
+
+;; fixtures
+(def filename "test-resources/readings.csv")
+(def ds-fixture (read-dataset filename :header true) )
+  
+;; only checks whether an non nil is returned
+(fact "perf-time-series-plot with ds and threshold"
+      (perf-time-series-plot  ds-fixture  3000)  => truthy)
+
+;; only checks whether an non nil is returned
+(fact "perf-histogram"
+      (perf-histogram  ds-fixture)  => truthy)
+
+;; only checks whether an non nil is returned
+(fact "count-bar-chart"
+      (count-bar-chart  ds-fixture :lb)  => truthy)
