7a0a35422a19656a5b90a337f9ca165983c9272e
Claude Falguiere <cfalguiere@gmail.com>
2013-06-01 15:51:21 +0200

    fixed an issue with midge

--- PtangDSL/Ptang/project.clj
+++ PtangDSL/Ptang/project.clj
@@ -2,8 +2,10 @@
   :description "Analysis tool for performance test results"
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [incanter "1.3.0"]]
-  :dev-dependencies [[swank-clojure "1.4.2"]
+  :dev-dependencies [[swank-clojure "1.4.2"][midje "1.4.0"]
 		     [lein-midje "1.0.10"][com.stuartsierra/lazytest "1.2.3"]]
   :repositories {"stuart" "http://stuartsierra.com/maven2"}
   :main ptang.core
-  )
+  ;:profiles {:dev {:plugins [[lein-midje "1.0.10"]]}
+  ;:plugins [[lein-midje "1.0.10"]]
+ )
