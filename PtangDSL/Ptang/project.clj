7208d3b38a390d03ac8f1aa67555c60f0c766f34
Claude Falguiere <cfalguiere@gmail.com>
2013-06-02 18:04:05 +0200

    added cli-time dependencie

--- PtangDSL/Ptang/project.clj
+++ PtangDSL/Ptang/project.clj
@@ -1,7 +1,9 @@
 (defproject ptang "0.1.0-SNAPSHOT"
   :description "Analysis tool for performance test results"
   :dependencies [[org.clojure/clojure "1.3.0"]
-		 [incanter "1.3.0"]]
+		 [incanter "1.3.0"] ;; statistics
+     [clj-time "0.4.4"] ;; joda time
+   ]
   :dev-dependencies [[swank-clojure "1.4.2"][midje "1.4.0"]
 		     [lein-midje "1.0.10"][com.stuartsierra/lazytest "1.2.3"]]
   :repositories {"stuart" "http://stuartsierra.com/maven2"}
