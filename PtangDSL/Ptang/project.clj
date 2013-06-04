782911ec319d10d3d0ab754a350a1b35184d0c58
Claude Falguiere <cfalguiere@gmail.com>
2013-06-04 22:38:01 +0200

    added license and url

--- PtangDSL/Ptang/project.clj
+++ PtangDSL/Ptang/project.clj
@@ -1,5 +1,10 @@
 (defproject ptang "0.1.1-SNAPSHOT"
   :description "Analysis tool for performance test results"
+  :url "https://github.com/cfalguiere/Ptangs"
+  :license {:name "Eclipse Public License 1.0"
+            :url "http://opensource.org/licenses/eclipse-1.0.php"
+            :distribution "repo"
+            :comments "Same license as Clojure"}
   :dependencies [[org.clojure/clojure "1.3.0"]
 		 [incanter "1.3.0"] ;; statistics
      [clj-time "0.4.4"] ;; joda time
